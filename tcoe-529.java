ec045f9b891f0742e1baf85380f66d233448b016
miklosmuller <81562893+miklosmuller@users.noreply.github.com>
2021-06-07 14:04:17 +0200

    Update tcoe-529.java

--- tcoe-529.java
+++ tcoe-529.java
@@ -1,5 +1,5 @@
 class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello, TCOE-529 with webhooks, take 16!"); 
+        System.out.println("Hello, TCOE-529 with webhooks, take 17!"); 
     }
 }
